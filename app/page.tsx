2f7c1ef06ddea16c8aa7fe079a8274f80ddce9d6
Peter Clark <peter.a.clark@accenture.com>
2026-08-31 17:04:15 +0100

    Add title to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,33 @@
-import { Button, Card, Grid, GridItem, Link } from "@chakra-ui/react";
+import { Button, Card, Grid, GridItem, Heading, Link } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap="6" padding="10">
-      <GridItem>
-        <Card.Root>
-          <Card.Body gap="2">
-            <Card.Title mb="2">Leaderboard</Card.Title>
-            <Card.Description>View the current Quiz Season Leaderboard</Card.Description>
-          </Card.Body>
-          <Card.Footer justifyContent="flex-end">
-            <Link href="/leaderboard"><Button>Go</Button></Link>
-          </Card.Footer>
-        </Card.Root>
-      </GridItem>
-      <GridItem>
-        <Card.Root>
-          <Card.Body gap="2">
-            <Card.Title mb="2">Enter Quiz</Card.Title>
-            <Card.Description>Enter the results of a quiz</Card.Description>
-          </Card.Body>
-          <Card.Footer justifyContent="flex-end">
-            <Link href="/leaderboard"><Button>Go</Button></Link>
-          </Card.Footer>
-        </Card.Root>
-      </GridItem>
-    </Grid>
+    <>
+      <Heading size="5xl" paddingLeft="10">Welcome to the Quiz Season App!</Heading>
+      <Grid templateColumns="repeat(2, 1fr)" gap="6" padding="10">
+        <GridItem>
+          <Card.Root>
+            <Card.Body gap="2">
+              <Card.Title mb="2">Leaderboard</Card.Title>
+              <Card.Description>View the current Quiz Season Leaderboard</Card.Description>
+            </Card.Body>
+            <Card.Footer justifyContent="flex-end">
+              <Link href="/leaderboard"><Button>Go</Button></Link>
+            </Card.Footer>
+          </Card.Root>
+        </GridItem>
+        <GridItem>
+          <Card.Root>
+            <Card.Body gap="2">
+              <Card.Title mb="2">Enter Quiz</Card.Title>
+              <Card.Description>Enter the results of a quiz</Card.Description>
+            </Card.Body>
+            <Card.Footer justifyContent="flex-end">
+              <Link href="/leaderboard"><Button>Go</Button></Link>
+            </Card.Footer>
+          </Card.Root>
+        </GridItem>
+      </Grid>
+    </>
   );
 }
