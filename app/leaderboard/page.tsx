0ecc08402c5711541676c2a24d630d135aa43017
Peter Clark <peter.a.clark@accenture.com>
2026-08-31 17:26:40 +0100

    Add leaderboard page with mock data

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,5 +1,60 @@
-import { Heading } from "@chakra-ui/react"
+import { Box, Heading, Table } from "@chakra-ui/react"
+
+type User = {
+    id: string;
+    position: string;
+    name: string;
+    total: number;
+    results: Record<string, number>
+}
+
+const mockData: User[] = [
+    {
+        id: 'abc123',
+        position: '1',
+        name: 'Chris',
+        total: 50,
+        results: {
+            '1': 5,
+            '2': 25,
+            '3': 20,
+        }
+    },
+    {
+        id: 'def456',
+        position: '2',
+        name: 'John',
+        total: 45,
+        results: {
+            '1': 5,
+            '2': 25,
+            '3': 15,
+        }
+    }
+]
 
 export default function Leaderboard() {
-    return <Heading>Leaderboard</Heading> 
+    return <>
+        <Heading size="5xl" paddingLeft="10">Leaderboard</Heading>
+        <Box padding="10">
+            <Table.Root size="lg">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeader>Position</Table.ColumnHeader>
+                        <Table.ColumnHeader>Name</Table.ColumnHeader>
+                        <Table.ColumnHeader>Score</Table.ColumnHeader>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {mockData.map((item) => (
+                        <Table.Row key={item.id}>
+                            <Table.Cell>{item.position}</Table.Cell>
+                            <Table.Cell>{item.name}</Table.Cell>
+                            <Table.Cell>{item.total}</Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+        </Box>
+    </>
 }
