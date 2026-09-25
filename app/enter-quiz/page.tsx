aabdefbc0ac0b086b4aef680d369597c9b863d44
Peter Clark <peter.a.clark@accenture.com>
2026-09-25 20:41:39 +0100

    Add delete and tie buttons

--- app/enter-quiz/page.tsx
+++ app/enter-quiz/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import { Card, Center, Field, Flex, Heading, Input, NativeSelect, Stack, Text } from "@chakra-ui/react";
+import { Button, Card, Center, Field, Flex, Heading, Input, NativeSelect, Stack, Text } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import { faHandshake, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const quizSeasons = [
     "2026 Quiz Season",
@@ -11,6 +14,17 @@ const quizSeasons = [
 const getTiedPosition = (entryPosition, positions) => {
     return 10
 }
+
+const IconButton = ({icon, action}: {icon: IconProp, action: () => void}) =>
+    <Button
+        cursor="pointer"
+        width="1/4"
+        marginLeft="1"
+        onClick={action}
+    >
+        <FontAwesomeIcon icon={icon} />
+    </Button>
+
 
 export default function ViewQuiz() {
     const [positions, setPositions] = useState<{ name: string, tied: boolean }[]>([])
@@ -22,6 +36,23 @@ export default function ViewQuiz() {
         setAvailableUsers(newAvailableUsers)
     }
 
+    const removeUser = (position: number) => {
+        const userName = positions[position].name
+        const newPositions = positions.toSpliced(position, 1)
+        setPositions(newPositions)
+        setAvailableUsers([...availableUsers, userName])
+    }
+
+    const tieUser = (name: string) => {
+        const newPositions = positions.map(pos => {
+            if (pos.name === name) {
+                return {name, tied: !pos.tied}
+            }
+            return pos
+        })
+        setPositions(newPositions)
+    }
+
     const positionsStackItems = []
     for (let i = 1; i <= 30; i++) {
         const entry = positions.length > i - 1 ? positions[i - 1] : undefined;
@@ -30,7 +61,11 @@ export default function ViewQuiz() {
             <Text>{position}</Text>
             {entry && <Card.Root w="1/2" alignItems={"center"} marginLeft="10" marginRight="10" key={entry.name} size="sm">
                 <Card.Body>
-                    <Center><Heading size="md">{entry.name}</Heading></Center>
+                    <Center>
+                        <Heading size="md">{entry.name}</Heading>
+                        <IconButton icon={faTrashCan} action={() => removeUser(i - 1)}></IconButton>
+                        <IconButton icon={faHandshake} action={() => tieUser(entry.name)}></IconButton>
+                    </Center>
                 </Card.Body>
             </Card.Root>}
         </Flex>
@@ -63,7 +98,7 @@ export default function ViewQuiz() {
                     marginRight="10"
                     key={name}
                     size="sm"
-                    _hover={{background: "#ADD8E6"}}
+                    _hover={{ background: "#ADD8E6" }}
                     onClick={() => addUser(i)}
                 >
                     <Card.Body>
